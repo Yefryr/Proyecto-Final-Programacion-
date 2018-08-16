ad373473b685a049fe78c57582fd590d0f040452
Yefryr <40300828+Yefryr@users.noreply.github.com>
2018-08-17 01:29:02 +0200

    Add files via upload

---
+++ Proyecto final/WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.EditarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Imagen_PredeterminadaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Estabilización_de_colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ArchivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AbrilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.GuardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.SalirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(62, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(549, 359);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.EditarToolStripMenuItem,
+            this.ArchivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.menuStrip1.Size = new System.Drawing.Size(121, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // EditarToolStripMenuItem
+            // 
+            this.EditarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Imagen_PredeterminadaToolStripMenuItem,
+            this.Estabilización_de_colorToolStripMenuItem});
+            this.EditarToolStripMenuItem.Name = "EditarToolStripMenuItem";
+            this.EditarToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.EditarToolStripMenuItem.Text = "Editar";
+            // 
+            // Imagen_PredeterminadaToolStripMenuItem
+            // 
+            this.Imagen_PredeterminadaToolStripMenuItem.Name = "Imagen_PredeterminadaToolStripMenuItem";
+            this.Imagen_PredeterminadaToolStripMenuItem.ShortcutKeyDisplayString = "";
+            this.Imagen_PredeterminadaToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D)));
+            this.Imagen_PredeterminadaToolStripMenuItem.Size = new System.Drawing.Size(243, 22);
+            this.Imagen_PredeterminadaToolStripMenuItem.Text = "Imagen Predeterminada";
+            this.Imagen_PredeterminadaToolStripMenuItem.Click += new System.EventHandler(this.Imagen_PredeterminadaToolStripMenuItem_Click);
+            // 
+            // Estabilización_de_colorToolStripMenuItem
+            // 
+            this.Estabilización_de_colorToolStripMenuItem.Name = "Estabilización_de_colorToolStripMenuItem";
+            this.Estabilización_de_colorToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.R)));
+            this.Estabilización_de_colorToolStripMenuItem.Size = new System.Drawing.Size(243, 22);
+            this.Estabilización_de_colorToolStripMenuItem.Text = "Estabilización de color";
+            this.Estabilización_de_colorToolStripMenuItem.Click += new System.EventHandler(this.Estabilización_de_colorToolStripMenuItem_Click);
+            // 
+            // ArchivoToolStripMenuItem
+            // 
+            this.ArchivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AbrilToolStripMenuItem,
+            this.GuardarToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.SalirToolStripMenuItem});
+            this.ArchivoToolStripMenuItem.Name = "ArchivoToolStripMenuItem";
+            this.ArchivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.ArchivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // AbrilToolStripMenuItem
+            // 
+            this.AbrilToolStripMenuItem.Name = "AbrilToolStripMenuItem";
+            this.AbrilToolStripMenuItem.ShortcutKeyDisplayString = "";
+            this.AbrilToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.AbrilToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.AbrilToolStripMenuItem.Text = "Abril";
+            this.AbrilToolStripMenuItem.Click += new System.EventHandler(this.AbrilToolStripMenuItem_Click);
+            // 
+            // GuardarToolStripMenuItem
+            // 
+            this.GuardarToolStripMenuItem.Name = "GuardarToolStripMenuItem";
+            this.GuardarToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.GuardarToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.GuardarToolStripMenuItem.Text = "Guardar Foto";
+            this.GuardarToolStripMenuItem.Click += new System.EventHandler(this.GuardarToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 6);
+            // 
+            // SalirToolStripMenuItem
+            // 
+            this.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem";
+            this.SalirToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.SalirToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.SalirToolStripMenuItem.Text = "Salir";
+            this.SalirToolStripMenuItem.Click += new System.EventHandler(this.SalirToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "JPEG;BMP;PNG|*.jpg;*.bmp;*.png";
+            this.openFileDialog1.Title = "Abrir foto";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Location = new System.Drawing.Point(274, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Sin foto seleccionada";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Title = "Guardar foto";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 594);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Color  de Fotos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ArchivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AbrilToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem GuardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem SalirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem EditarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Imagen_PredeterminadaToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem Estabilización_de_colorToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Proyecto final/WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //
+        //Creacion de Barras de colores
+        //
+        static int CNT = 4;
+        public static List<TrackBar> tbs = new List<TrackBar>();
+        Color[] clr = new Color[] { Color.Red, Color.Green, Color.Blue, Color.Yellow };
+        string[] lb = new string[] { "Rojo", "Verde", "Azul", "Amarillo" };
+        Image img2, img_org;
+
+        private static Image Llenar_Leyer(PictureBox PicBox, Image img, Color[] colr)
+        {
+            
+            PicBox.Image = img;
+            Bitmap bmp_img = new Bitmap(PicBox.Image);
+            Graphics ghp = Graphics.FromImage(bmp_img);
+            LinearGradientBrush LineaBrush;
+            for (int i = 0; i < CNT; i++)
+            {
+                LineaBrush = new LinearGradientBrush(new Rectangle(0, 0, bmp_img.Width, bmp_img.Height), Color.FromArgb(tbs[i].Value, colr[i]), Color.FromArgb(tbs[i].Value, colr[i]), LinearGradientMode.BackwardDiagonal);
+                ghp.FillRectangle(LineaBrush, new Rectangle(0, 0, bmp_img.Width, bmp_img.Height));
+            }
+            return (Image)bmp_img;
+        }
+
+        private void Imagen_PredeterminadaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < CNT; i++)
+            {
+                tbs[i].Value = 0;
+            }
+            pictureBox1.Image = img2 = img_org;
+        }
+
+        private void GuardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Guardar fotos//
+            saveFileDialog1.Filter = "JPEG File (*.jpg)|*.jpg|Bitmap File (*.bmp)|*.bmp|PNG File(*.png)|*.png";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                switch (Path.GetExtension(saveFileDialog1.FileName))
+                {
+                    case ".jpg":
+                        pictureBox1.Image.Save(saveFileDialog1.FileName, ImageFormat.Jpeg); break;
+                    case ".bmp":
+                        pictureBox1.Image.Save(saveFileDialog1.FileName, ImageFormat.Bmp); break;
+                    case ".png":
+                        pictureBox1.Image.Save(saveFileDialog1.FileName, ImageFormat.Png); break;
+                }
+            }
+        }
+
+        private void SalirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Salir
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            add_tracks();
+        }
+        void add_tracks()
+        {
+            //Procedimiento para las barras de colores dar location y size
+            for (int i = 0; i < CNT; i++)
+            {
+                TrackBar tb = new TrackBar();
+                tb.Size = new Size(114, 45);
+                tb.Location = new Point(i * 114 + 214, 545);
+                tb.Enabled = false;
+                tb.Minimum = 0;
+                tb.Maximum = 255;
+                tb.Value = 0;
+                tb.TickStyle = TickStyle.None;
+                tb.ValueChanged += new EventHandler(tb_ValueChanged);
+                tbs.Add(tb);
+
+                Label l = new Label();
+                l.Text = lb[i];
+                l.Location = new Point(i * 114 + 260, 567);
+                this.Controls.Add(l);
+            }
+            this.Controls.AddRange(tbs.ToArray());
+            this.Width += (CNT - 4) * 110;
+            label1.Left = (int)((pictureBox1.Width - label1.Width) / 2);
+        }
+
+        private void AbrilToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Procedimiento Abrir Imagen 
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    label1.Visible = false;
+                    pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                    img_org = img2 = pictureBox1.Image;
+                    for (int i = 0; i < CNT; i++)
+                    {
+                        tbs[i].Enabled = true;
+                    }
+                }
+            }
+            catch
+            {
+                pictureBox1.Image = null;
+                label1.Visible = true;
+                for (int i = 0; i < CNT; i++)
+                {
+                    tbs[i].Enabled = false;
+                }
+            }
+        }
+
+        private void Estabilización_de_colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Barras de colores vuelven a cero
+            img2 = pictureBox1.Image;
+            for (int i = 0; i < CNT; i++)
+            {
+                tbs[i].Value = 0;
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        void tb_ValueChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < CNT; i++)
+            {
+                toolTip1.SetToolTip(tbs[i], tbs[i].Value.ToString());
+            }
+            pictureBox1.Image = Llenar_Leyer(pictureBox1, img2, clr);
+        }
+    }
+
+}
